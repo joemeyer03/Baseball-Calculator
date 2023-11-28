9905966e242fb23c955f3a860ea5b52383bf3f17
Joe Meyer <josephmeyer03.jm@gmail.com>
2023-11-28 00:38:40 -0500

    Got playoff predictor working and updated readme

--- BaseballCalc.py
+++ BaseballCalc.py
@@ -2,29 +2,60 @@
 
 import os
 import math
+import time
 from statistics import NormalDist
 
+## These stats are based off the last 5 years of playoffs
+## MLB changed the playoff format in 2022, but I chose to include 2018 to now
+## to create a larger sample size and I retroactively added teams to the playoffs.
+## Also ignored the 2020 season which was shortened due to covid
 avg_wins_of_playoff_team = 94.82
 stdev_of_wins_of_playoff_team = 6.76
 
 ### Takes a teams runs scored and their runs allowed and calculates what their
 ### win percentage should be based off Bill James pythagorean win percentage
-def pythag_wins(runs_scored, runs_allowed) -> float:
+def __pythag_wins(runs_scored, runs_allowed) -> float:
     return 1 / (1 + (runs_allowed / runs_scored)**2)
 
-### Takes two teams winning percentage and returns the probability that A beats B
-def head_to_head_percent(win_percent_of_A, win_percent_of_B):
-    numerator = win_percent_of_A - win_percent_of_A * win_percent_of_B
-    denominator = win_percent_of_A + win_percent_of_B - 2 * win_percent_of_A * win_percent_of_B
+### Takes two percentages and returns the probability that A beats B
+def __head_to_head_percent(A_percent, percent_B):
+    numerator = A_percent - A_percent * percent_B
+    denominator = A_percent + percent_B - 2 * A_percent * percent_B
     return  numerator / denominator
 
 ### Takes two teams win totals and returns the probability that A beats B
-def head_to_head_wins_losses(A_wins, B_wins):
+def __head_to_head_wins_losses(A_wins, B_wins):
     A_losses = 162 - A_wins
     B_losses = 162 - B_wins
 
     return (A_wins * B_losses) / (A_wins * B_losses + B_wins * A_losses)
 
+### Takes two teams win totals and a series length and figures out the odds that team A wins the series
+### Calculates prob that team A beats team B 
+### Then determines if a team would win the majority of a 7 game series
+def __prob_to_win_series(A_wins, B_wins, series_length):
+    wins_needed = series_length // 2 + 1
+    A_wins_game = __head_to_head_wins_losses(A_wins, B_wins)
+    A_wins_series = 0
+
+    for i in range(wins_needed, series_length + 1):
+        A_wins_series += (A_wins_game ** i) * (1-A_wins_game) ** (series_length - i) * math.comb(series_length, i)
+    return A_wins_series
+
+### Takes two teams and returns which team wins the series
+def __run_series(runs, teams, A, B, series_length) -> int:
+    A_wins = __pythag_wins(runs[A][0], runs[A][1])
+    B_wins = __pythag_wins(runs[B][0], runs[B][1])
+    A_beats_B = round(__prob_to_win_series(A_wins, B_wins, series_length)*100, 1)
+    time.sleep(1)
+    if (A_beats_B >= 50): 
+        print("The " + teams[A] + " have a " + str(A_beats_B) + "% chance of beating the " + teams[B] + ".")
+        return A
+    else:
+        print("The " + teams[B] + " have a " + str(100 - A_beats_B) + "% chance of beating the " + teams[A] + ".")
+        return B
+
+### Asks a player for either wins/losses or runs scored/allowed and determines if the team should make the playoffs
 def make_playoffs() -> None:
     print("Would you rather: \
             \n\t1. Determine if your team will make it based off its current \
@@ -43,7 +74,7 @@ def make_playoffs() -> None:
             inp_loop = True
             runs_scored = int(input("Enter current runs scored: "))
             runs_allowed = int(input("Enter current runs allowed: "))
-            percent = pythag_wins(runs_scored, runs_allowed)
+            percent = __pythag_wins(runs_scored, runs_allowed)
         else:
             print("Try again.")
             inp = input("Enter input: ")
@@ -61,38 +92,81 @@ def make_playoffs() -> None:
     else:
         print("So, your team will not make the playoffs")
 
-def runs_playoffs() -> None:
-    pass
-
+## Asks for how many runs team has scored and allowed and asks for actual win percent
+## Outputs projected number of games team is going to win more than they should
 def lucky() -> None:
     pass
 
+## Asks user for teams current win percentage and competitors current win percentage
+#  Determines who will win 
 def who_wins() -> None:
     pass
 
+## Asks user for batter's batting average and pitcher's batting average against and determines who would win
 def pitch_v_hitter() -> None:
     pass
 
+## Asks user for teams in playoffs and their run totals and determines who would win the whole thing
 def playoff_predictor() -> None:
     inp_help = ["first", "second", "third", "fourth", "fifth", "sixth"]
-
-    american = dict()
-    national = dict()
-
-    print(head_to_head_percent(.767, .361))
-    print(head_to_head_wins_losses(.767*162, .361*162))
-
-    playoffs = list()
+    
+    american = list()
+    american_runs = list(list())
+    national = list()
+    national_runs = list(list())
 
     for i in inp_help:
         name = input("Enter name of " + i + " place American League team: ")
-        wins = input("Enter win total of " + name + ": ")
-        american[name] = wins
+        runs_for = int(input("Enter runs scored by the " + name + ": "))
+        runs_against = int(input("Enter runs scored against the " + name + ": "))
+        american.append(name)
+        american_runs.append([runs_for, runs_against])
     for i in inp_help:
         name = input("Enter name of " + i + " place National League team: ")
-        wins = input("Enter win total of " + name + ": ")
-        national[name] = wins
+        runs_for = int(input("Enter runs scored by the " + name + ": "))
+        runs_against = int(input("Enter runs scored against the " + name + ": "))
+        national.append(name)
+        national_runs.append([runs_for, runs_against])
+    print("\n\n------------------------Playoff Simulation Based off Wins------------------------\n")
+    print("\n---------------------------------Wild Card Games---------------------------------\n")
+    # who wins American Leauge wild card games
+    face_two_seed_a = __run_series(american_runs, american, 2, 5, 3)
+    face_one_seed_a = __run_series(american_runs, american, 3, 4, 3)
+    # who wins National League wild card games
+    face_two_seed_n = __run_series(national_runs, national, 2, 5, 3)
+    face_one_seed_n = __run_series(national_runs, national, 3, 4, 3)
 
+    time.sleep(2)
+    print("\n--------------------------------------ALDS---------------------------------------\n")
+    # who wins ALDSs
+    alcs_team_one = __run_series(american_runs, american, 1, face_two_seed_a, 5)
+    alcs_team_two = __run_series(american_runs, american, 0, face_one_seed_a, 5)
+    time.sleep(2)
+    print("\n--------------------------------------NLDS---------------------------------------\n")
+    # who wins NLDSs
+    nlcs_team_one = __run_series(national_runs, national, 1, face_two_seed_n, 5)
+    nlcs_team_two = __run_series(national_runs, national, 0, face_one_seed_n, 5)
+
+    time.sleep(2)
+    print("\n--------------------------------------ALCS---------------------------------------\n")
+    # who wins ALCS
+    american_ws_team = __run_series(american_runs, american, alcs_team_one, alcs_team_two, 7)
+    time.sleep(2)
+    print("\n--------------------------------------NLCS---------------------------------------\n")
+    # who wins NLCS
+    national_ws_team = __run_series(national_runs, national, nlcs_team_one, nlcs_team_two, 7)
+    
+    time.sleep(2)
+    print("\n----------------------------------World Series-----------------------------------\n")
+    # who wins World Series
+    nl_wins = __pythag_wins(national_runs[national_ws_team][0], national_runs[national_ws_team][1])
+    al_wins = __pythag_wins(american_runs[american_ws_team][0], american_runs[american_ws_team][1])
+    A_beats_B = round(__prob_to_win_series(nl_wins, al_wins, 7)*100, 1)
+    time.sleep(3)
+    if (A_beats_B > .5):
+        print("The " + national[national_ws_team] + " have a " + str(A_beats_B) + "% chance of beating the " + american[american_ws_team] + ", so they win the World Series!")
+    else:
+        print("The " + american[american_ws_team] + " have a " + str(100 - A_beats_B) + "% chance of beating the " + national[national_ws_team] + ", so they win the World Series!")
     
 
 def help():
@@ -110,11 +184,10 @@ def help():
 command_blueprints = {
     help : ["Help", "0"],
     make_playoffs : ["Should my team make the playoffs?","1"],
-    runs_playoffs : ["How many runs does my team need to make the playoffs?","2"],
-    lucky : ["Is my team lucky?","3"],
-    who_wins : ["Is my team going to win this game?"],
-    pitch_v_hitter : ["Who wins between a pitcher and batter?"],
-    playoff_predictor : ["Playoff Predictor","4"],
+    lucky : ["Is my team lucky?","2"],
+    who_wins : ["Is my team going to win this game?","3"],
+    pitch_v_hitter : ["Who wins between a pitcher and batter?","4"],
+    playoff_predictor : ["Playoff Predictor","6"],
 }
 commands = {}
 
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Baseball-Calculator
-Attempts to calculate certain baseball questions using statistics
+Attempts to calculate certain baseball questions using statistics. Answers these questions so far:
+
+## Should my team make the playoffs?
+- Asks the user to choose between entering wins/losses or runs scored/allowed and determines if the team should make the playoffs.
+- If the user opts to use runs, the program will calculate the expected wins using the [pythagorean expectation formula](https://en.wikipedia.org/wiki/Pythagorean_expectation).
+- I determine if a team makes the playoffs by determining how far away the win total is from the mean wins a playoff team usually has.
+- I determined that the average wins of a playoff team is 94.82 and the standard deviation is 6.76 
+    - MLB changed the playoff format in 2022 to include more teams, but I chose to include back to 2018, I retroactively added teams to the playoffs, to create a larger sample size.
+    - I also ignored the 2020 season which was shortened due to covid
+
+## Is my team lucky?
+- Not currently implemented
+- Will ask for how many runs team has scored and allowed and ask for actual win percent
+- Outputs projected number of games team is going to win more than they should
+- Also uses pythagorean expectation
+
+## Is my team going to win this game?
+- Not currently implemented
+- Will ask user for teams current win percentage and competitors current win percentage
+- Determines who will win using Bill James's [Log5](https://en.wikipedia.org/wiki/Log5) formula
+
+## Who wins between a pitcher and batter?
+- Not currently implemented
+- Will ask user for batter's batting average and pitcher's batting average against and determines who would win
+- Also uses Log5
+
+## Who would win the World Series?
+- Asks user for teams in playoffs and their run totals and determines who would win the whole thing
+- Uses pythagorean expectation to determine how many teams a team should have actually got based off their run totals
+- Using those wins, program determines who would win a series of 3, 5, or 7 games
+    - First, program determines the probability that a team wins one game using the Log5 formula, will call that P(A)
+    - Then, uses this equation to determine probability that a team wins majority of the games in a series (this equation is for a seven game series, where one team needs to win 4 games):
+```math
+\sum_{n=4}^7 P(A) ^ n \cdot (1-P(A))^{7-n} \cdot {7 \choose n}
+```
+- There is a file called "2023playoffs.txt", which has the proper inputs for the 2023 MLB playoffs, if you'd like to see the prediction and probabilities
